9b7ddc1a1b3e74c984112ee6eb58815143c21144
jian hua <jianhua282721@gmail.com>
2020-03-05 22:40:36 +0800

    Updates

---
+++ precache-manifest.a287d2d8bc93f16f84f829e0948f646d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276705266dc6febe8e1512e490043de6",
+    "url": "/kyjane/freshfarm/index.html"
+  },
+  {
+    "revision": "b6a6c15c44fbd399f19c",
+    "url": "/kyjane/freshfarm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "324cd4f0515c6810ce15",
+    "url": "/kyjane/freshfarm/static/js/2.316e974c.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/kyjane/freshfarm/static/js/2.316e974c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6a6c15c44fbd399f19c",
+    "url": "/kyjane/freshfarm/static/js/main.6534f357.chunk.js"
+  },
+  {
+    "revision": "0a02808afc24e1c28e3b",
+    "url": "/kyjane/freshfarm/static/js/runtime-main.e5fead24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kyjane/freshfarm/static/media/logo.5d5d9eef.svg"
+  }
+]);
